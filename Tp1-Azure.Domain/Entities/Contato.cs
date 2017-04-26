150bb9f79817817867efc43a8e42587506341935
Rodrigo Filomeno <Rodrigo Filomeno>
2017-04-26 16:25:33 -0300

    First Commit

---
+++ Tp1-Azure.Domain/Entities/Contato.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tp1_Azure.Domain.Entities
+    {
+    public class Contato : TableEntity
+    {
+        public Contato(string nome, string sobrenome, int telefone, string email)
+        {
+            this.PartitionKey = nome;
+            this.RowKey = sobrenome;
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+            this.Telefone = telefone;
+            this.Email = email;
+        }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Tp1-Azure.Presentation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tp1_Azure.Domain.Entities;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Table; // Namespace for Table storage types
+
+namespace Tp1_Azure.Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva o caminho (path) do arquivo csv: ");
+            var pathCsv = Console.ReadLine();
+
+            IList<Contato> contatos = new List<Contato>();
+
+            
+            var contatosCsv = File.ReadAllLines(pathCsv);
+            
+            foreach (var contatoNaoFormatado in contatosCsv)
+            {
+                var dados = contatoNaoFormatado.Split(',');
+                Contato contato = new Contato(dados[0], dados[1], Convert.ToInt32(dados[2]), dados[3]);
+
+                contatos.Add(contato);
+            }
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("contato");
+
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+
+            foreach (var contato in contatos)
+            {
+                TableOperation insertOperation = TableOperation.Insert(contato);
+                table.Execute(insertOperation);
+            }
+
+            foreach (var contato in contatos)
+            {
+                Console.WriteLine("{0} {1}",contato.Nome, contato.Sobrenome);
+                Console.WriteLine(contato.Telefone);
+                Console.WriteLine(contato.Email);
+                Console.WriteLine("-----------------------------------------");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
