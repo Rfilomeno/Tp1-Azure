44c68a71cd6c621e9496b922f16c0e5ab2c78b57
Rodrigo Filomeno <rodrigo.filomeno@al.infnet.edu.br>
2017-04-27 20:09:27 -0300

    colocando blob

--- Tp1-Azure.Presentation/Program.cs
+++ Tp1-Azure.Presentation/Program.cs
@@ -8,6 +8,7 @@ using Tp1_Azure.Domain.Entities;
 using Microsoft.Azure; // Namespace for CloudConfigurationManager
 using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
 using Microsoft.WindowsAzure.Storage.Table; // Namespace for Table storage types
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Tp1_Azure.Presentation
 {
@@ -16,13 +17,13 @@ namespace Tp1_Azure.Presentation
         static void Main(string[] args)
         {
             Console.Write("Escreva o caminho (path) do arquivo csv: ");
-            var pathCsv = Console.ReadLine();
-
+            //var pathCsv = Console.ReadLine();
+            var pathCsv = @"C:\workspace\Tp1-Azure\Data\Contatos.csv";
             IList<Contato> contatos = new List<Contato>();
 
-            
+
             var contatosCsv = File.ReadAllLines(pathCsv);
-            
+
             foreach (var contatoNaoFormatado in contatosCsv)
             {
                 var dados = contatoNaoFormatado.Split(',');
@@ -30,6 +31,49 @@ namespace Tp1_Azure.Presentation
 
                 contatos.Add(contato);
             }
+
+            //CriandoTabelaNoStorage(contatos);
+
+            //CriandoBlobNoStorage(pathCsv);
+
+            foreach (var contato in contatos)
+            {
+                Console.WriteLine("{0} {1}", contato.Nome, contato.PartitionKey);
+                Console.WriteLine(contato.RowKey);
+                Console.WriteLine(contato.Email);
+                Console.WriteLine("-----------------------------------------");
+            }
+            Console.ReadKey();
+
+        }
+
+        private static void CriandoBlobNoStorage(string pathCsv)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("rodrigofilomeno");
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("contato.csv");
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(pathCsv))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+        }
+
+        private static void CriandoTabelaNoStorage(IList<Contato> contatos)
+        {
             // Retrieve the storage account from the connection string.
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
                 CloudConfigurationManager.GetSetting("StorageConnectionString"));
@@ -50,16 +94,6 @@ namespace Tp1_Azure.Presentation
                 TableOperation insertOperation = TableOperation.Insert(contato);
                 table.Execute(insertOperation);
             }
-
-            foreach (var contato in contatos)
-            {
-                Console.WriteLine("{0} {1}",contato.Nome, contato.PartitionKey);
-                Console.WriteLine(contato.RowKey);
-                Console.WriteLine(contato.Email);
-                Console.WriteLine("-----------------------------------------");
-            }
-            Console.ReadKey();
-            
         }
     }
 }
